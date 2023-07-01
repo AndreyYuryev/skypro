4d5ba39957076b79a685a3ba1731d3bc00a793bb
Andrey <yuryev@yandex.ru>
2023-07-01 15:16:40 +0300

    words

---
+++ Sources/sample.py
@@ -0,0 +1,12 @@
+summ = int(input())
+
+# Половина
+half = summ/2
+# Четверть
+quarter = summ / 4
+# Десятая часть
+decimal = 0.1 * summ
+
+print("Половина", half)
+print("Четверть", quarter)
+print("Десятая", decimal)
