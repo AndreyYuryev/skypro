ce0ef6518aa61e02c57564de66020a3712d1286d
Andrey <yuryev@yandex.ru>
2023-08-01 16:00:44 +0300

    setup.py

---
+++ tests/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='skypro',
+    version='0.1',
+    packages=['validators'],
+    package_dir={'sources': 'sources', 'test': 'tests'},
+    url='',
+    license='',
+    author='Andrey Yuryev',
+    author_email='',
+    description='SkyPro cources'
+)
