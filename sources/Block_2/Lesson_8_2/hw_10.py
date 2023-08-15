507dc80884dd7d74c3fd51c3fca791d3b9bff1e4
Andrey <yuryev@yandex.ru>
2023-08-16 01:09:05 +0300

    Lesson 8.2 hw's

---
+++ sources/Block_2/Lesson_8_2/hw_10.py
@@ -0,0 +1,14 @@
+class Hero():
+
+    def __init__(self, name, posessions):
+        self.name = name
+        self.posessions = posessions
+
+    def __repr__(self):
+        things = ", ".join(self.posessions)
+        return f"Герой {self.name} взял с собой {things}"
+
+# Не удаляйте код ниже, он нужен для проверки
+
+hero = Hero("Питомир", ["меч", "плащ", "шляпа"])
+print(hero)
