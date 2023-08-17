64438789c1e25f79aafb2b6c527b774d8910cab5
Andrey <yuryev@yandex.ru>
2023-08-17 17:48:48 +0300

    samples

--- sources/Block_2/Tasks/gramota.py
+++ sources/Block_2/Tasks/gramota.py
@@ -1,7 +1,11 @@
 import requests
 import json
+import pprint
+import urllib3
 
+urllib3.disable_warnings()
 response = requests.get(url='https://jsonkeeper.com/b/LJHI', verify=False)
 
 data = response.json()
-print(data)
+pp = pprint.PrettyPrinter(indent=4)
+pp.pprint(data)
